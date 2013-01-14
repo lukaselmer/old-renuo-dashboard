1e3cbc54a4f56c07ac1f213ddde03dd83cee0447
Lukas Elmer <lukas.elmer@gmail.com>
2013-01-14 19:41:34 +0100

    initial commit

---
+++ js/dashboard.js
@@ -0,0 +1,14 @@
+$(function () {
+    var links = {
+        "Renuo":"redmine.renuo.ch",
+        "Shariando":"redmine.renuo.ch/projects/shariando/issues",
+        "Studentbooks":"studentbooks.redmine.renuo.ch"
+    };
+
+    $.each(links, function (name, href) {
+        var link = $("<a></a>");
+        link.attr("href", "https://" + href);
+        link.text(name);
+        $("#redmine").append($("<li></li>").append(link));
+    });
+});
