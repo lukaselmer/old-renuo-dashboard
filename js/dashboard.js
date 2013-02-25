d98ea9d21aad23765c3bd49c6a62d0463a8d2b92
Lukas Elmer <lukas.elmer@gmail.com>
2013-02-25 11:37:31 +0100

    added jira

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,8 @@ $(function () {
     var links = {
         "Renuo":"redmine.renuo.ch",
         "Shariando":"redmine.renuo.ch/projects/shariando/issues",
-        "Studentbooks":"studentbooks.redmine.renuo.ch"
+        "Studentbooks":"studentbooks.redmine.renuo.ch",
+        "JIRA":"https://renuogmbh.atlassian.net"
     };
 
     $.each(links, function (name, href) {
