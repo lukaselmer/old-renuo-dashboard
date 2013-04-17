e7be7507be3cba20bca7dcd32946c265c029f118
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-17 12:14:35 +0300

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -16,7 +16,7 @@ $(function () {
     });
 
     fillList("meetings", {
-        "Redmine Stunden": "https://redmine.renuo.ch/time_entries/report?utf8=%E2%9C%93&criteria%5B%5D=project&criteria%5B%5D=user&criteria%5B%5D=issue&f%5B%5D=spent_on&op%5Bspent_on%5D=%3E%3Ct-&v%5Bspent_on%5D%5B%5D=7&f%5B%5D=&c%5B%5D=project&c%5B%5D=spent_on&c%5B%5D=user&c%5B%5D=activity&c%5B%5D=issue&c%5B%5D=comments&c%5B%5D=hours&columns=week",
         "Sitzungsprotokoll": "https://docs.google.com/a/renuo.ch/document/d/1yvhmygDaHKDRmeHtL2wE5NS8MNi3FmSdnelt0sQJxo8/edit#",
+        "Redmine Stunden": "https://redmine.renuo.ch/time_entries/report?utf8=%E2%9C%93&criteria%5B%5D=project&criteria%5B%5D=user&criteria%5B%5D=issue&f%5B%5D=spent_on&op%5Bspent_on%5D=%3E%3Ct-&v%5Bspent_on%5D%5B%5D=7&f%5B%5D=&c%5B%5D=project&c%5B%5D=spent_on&c%5B%5D=user&c%5B%5D=activity&c%5B%5D=issue&c%5B%5D=comments&c%5B%5D=hours&columns=week",
     });
 });
