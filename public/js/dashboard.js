6fcb62c8bb8c0bb3014417e733228131ff0dda94
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-27 09:52:41 +0100

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -18,7 +18,7 @@ $(function () {
     fillList("meetings", {
         "Sitzungsprotokoll": "docs.google.com/a/renuo.ch/document/d/1Ktr9rWkbVlIDZsDFRlrYp3_gmiikf5n0Z-OROa7bkgI/edit",
         "Redmine Stunden": "redmine.renuo.ch/time_entries/report?utf8=%E2%9C%93&criteria%5B%5D=project&criteria%5B%5D=user&criteria%5B%5D=issue&f%5B%5D=spent_on&op%5Bspent_on%5D=%3E%3Ct-&v%5Bspent_on%5D%5B%5D=7&f%5B%5D=&c%5B%5D=project&c%5B%5D=spent_on&c%5B%5D=user&c%5B%5D=activity&c%5B%5D=issue&c%5B%5D=comments&c%5B%5D=hours&columns=week",
-        "Float Schedule": "https://renuo.floatschedule.com/"
+        "Float": "https://renuo.floatschedule.com/"
     });
     
     fillList("hotfeet", {
