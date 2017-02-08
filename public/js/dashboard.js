1fa4ab4dcece34b37f34cfe802037322d701fcf3
Alessandro Rodi <coorasse@gmail.com>
2017-02-08 08:55:43 +0100

    add a new investment time section

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,4 @@
 $(function () {
-    
     (function(){
         var fillRedmineList = function(id, list){
             $.each(list, function (index, str_id_and_text) {
@@ -13,7 +12,17 @@ $(function () {
                 $("#" + id).append($("<li></li>").append(redmine_id + ": ").append(link));
             });
         };
-        
+
+        var fillList = function(id, list){
+            $.each(list, function (name, href) {
+                var link = $("<a></a>");
+                if (href.indexOf("://") == -1) href = "https://" + href;
+                link.attr("href", href);
+                link.text(name);
+                $("#" + id).append($("<li></li>").append(link));
+            });
+        };
+
         fillRedmineList("redmine_issues", [
             "1339 Wöchentliche Meetings", 
             "1592 Büro",
@@ -29,47 +38,41 @@ $(function () {
             "2949 Krankheit",
             "3704 Scrum Team 11",
             "4999 Scrum Team Griffin",
-            "5129 Investments Team 11",
-            "5000 Investments Team Griffin",
             "1542 renuo.ch Seitenpflege & Updates"
         ]);
-    })();
-    
-    
-    (function(){
-        var fillList = function(id, list){
-            $.each(list, function (name, href) {
-                var link = $("<a></a>");
-                if (href.indexOf("://") == -1) href = "https://" + href;
-                link.attr("href", href);
-                link.text(name);
-                $("#" + id).append($("<li></li>").append(link));
-            });
-        };
-    
+
         fillList("redmine", {
             "Renuo":"redmine.renuo.ch",
             "My Tickets":"redmine.renuo.ch/issues?query_id=14",
             "Studentbooks":"redmine-studentbooks.renuo.ch",
             "Luke's Tickets":"redmine.renuo.ch/issues?query_id=21",
             "Wiki":"redmine.renuo.ch/projects/internal/wiki",
-            "Rails Application Configuration":"redmine.renuo.ch/projects/internal/wiki/Application_Configuration"
+            "Rails Application Configuration":"renuo.gitbooks.io/rails-application-setup-guide/content/application_configuration.html"
         });
-    
+
         fillList("meetings", {
             "Sitzungsprotokoll": "docs.google.com/a/renuo.ch/document/d/1Y7g28aGsVW_r_yqWDgkUl8LpA4y-9OEzjStGy7TObyU/edit",
             "Redmine Stunden": "redmine.renuo.ch/time_entries/report?utf8=%E2%9C%93&criteria%5B%5D=project&criteria%5B%5D=user&criteria%5B%5D=issue&f%5B%5D=spent_on&op%5Bspent_on%5D=%3E%3Ct-&v%5Bspent_on%5D%5B%5D=7&f%5B%5D=&c%5B%5D=project&c%5B%5D=spent_on&c%5B%5D=user&c%5B%5D=activity&c%5B%5D=issue&c%5B%5D=comments&c%5B%5D=hours&columns=week",
             "Float": "renuo.floatschedule.com"
         });
-        
+
         fillList("hotfeet", {
             "Wiki": "redmine.renuo.ch/projects/internal/wiki/HotFeet",
             "Jira": "jira.hotfeet.ch",
             "Ldap": "ldap.hotfeet.ch",
             "Ldap Doku": "docs.google.com/presentation/d/1_WY3MeUFXCbjbiE4d84MyjddFq6qRfpdJZXBYztWTJE/edit#slide=id.p",
             "Git": "git.hotfeet.ch",
-            "SSH Devel": "ssh://devel.hotfeet.ch",
+            "SSH Devel": "ssh://devel.hotfeet.ch"
         });
+
+        fillList("investment_time", {
+            "Documentation and specification": "docs.google.com/a/renuo.ch/document/d/17M9T0opwtB9Kz0sOG22--K_ZJrR1KK7IgHSQJOna3KY/edit?usp=sharing",
+        });
+
+        fillRedmineList("investment_time", [
+            "7777 Investments Team 11",
+            "5000 Investments Team Griffin"
+        ]);
     })();
     
     $("#current_year").text(new Date().getUTCFullYear());
